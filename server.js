dbf574a8e852a476e81def23b5bb67e8d0ea532d
Roz <roz_E4@hotmail.com>
2020-07-20 20:22:06 +0800

    models

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+//Add middleware
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+//API routes
 
 // Send every request to the React app
 // Define any API routes before this runs
